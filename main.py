fdfdb181410696946ba937d37072b36722c4d2a9
Đại Nguyễn Quốc <17521294@gm.uit.edu.vn>
2020-09-15 13:43:59 +0700

    fixing error

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ from models import user
 from db.database import SessionLocal, engine
 from api.api_v1.api import api_router
 user.Base.metadata.create_all(bind=engine)
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
     response = Response("Internal server error", status_code=500)
